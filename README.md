592e67c213148d1367989cbc6f9181fc4b91d4f1
David Curthoys <david.curthoys@googlemail.com>
2022-02-04 17:00:31 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,201 @@
+# Child Commands
+
+Extension trait / facade on EntityCommands that allows you to 
+spawn entity hierarchies without nesting or collecting the ids.
+
+# Examples
+
+## Before:
+
+```rust
+fn setup(mut commands: Commands) {
+    commands
+        .spawn_bundle(PbrBundle {
+            transform: Transform::from_xyz(1.0, 1.0, 1.0),
+            ..Default::default()
+        })
+        .with_children(|parent| {
+            parent
+                .spawn_bundle(PbrBundle {
+                    transform: Transform::from_xyz(1.0, 1.0, 1.0),
+                    ..Default::default()
+                })
+                .spawn_bundle(PbrBundle {
+                    transform: Transform::from_xyz(2.0, 2.0, 2.0),
+                    ..Default::default()
+                })
+        });
+}
+```
+
+## or
+
+```rust
+fn setup(mut commands: Commands) {
+    let child_1 = commands
+        .spawn_bundle(PbrBundle {
+            transform: Transform::from_xyz(1.0, 1.0, 1.0),
+            ..Default::default()
+        })
+        .id();
+    let child_2 = commands
+        .spawn_bundle(PbrBundle {
+            transform: Transform::from_xyz(2.0, 2.0, 2.0),
+            ..Default::default()
+        })
+        .id();
+    commands
+        .spawn_bundle(PbrBundle {
+            transform: Transform::from_xyz(1.0, 1.0, 1.0),
+            ..Default::default()
+        })
+        .push_child(child_1)
+        .push_child(child_1);
+}
+```
+
+## After:
+
+```rust
+use child_commands::*;
+
+fn setup(mut commands: Commands) {
+    commands
+        .spawn_bundle(PbrBundle {
+            transform: Transform::from_xyz(1.0, 1.0, 1.0),
+            ..Default::default()
+        })
+        .spawn_child_bundle(PbrBundle {
+            transform: Transform::from_xyz(1.0, 1.0, 1.0),
+            ..Default::default()
+        })
+        .spawn_sibling_bundle(PbrBundle {
+            transform: Transform::from_xyz(2.0, 2.0, 2.0),
+            ..Default::default()
+        });
+}
+```
+#
+
+##Before:
+
+```rust
+pub fn spawn_text_box(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+) {
+ 
+    commands.spawn_bundle(NodeBundle {
+        style: Style {
+            position_type: PositionType::Absolute,
+            position: Rect { left: Val::Px(100.0), bottom: Val::Px(100.0), ..Default::default() },
+            padding: Rect::all(Val::Px(2.0)),
+            ..Default::default()
+        },
+        ..Default::default()
+    })
+    .with_children(|builder| {
+        builder.spawn_bundle(NodeBundle {
+                color: UiColor (Color::DARK_GRAY),
+                style: Style {
+                    padding: Rect::all(Val::Px(4.0)),
+                    ..Default::default()
+                },
+                ..Default::default()
+            }
+        )
+        .with_children(|builder| {
+            builder.spawn_bundle(TextBundle {
+                text: Text::with_section(
+                    "Hello, world!",
+                    TextStyle {
+                        font: asset_server.load("FiraMono-Regular.ttf"),
+                        font_size: 16.0,
+                        color: Color::ANTIQUE_WHITE,
+                    },
+                    TextAlignment::default()
+                ),
+                 ..Default::default()
+            });
+        });
+    });
+}
+```
+
+## After:
+
+```rust
+use child_commands::*;
+
+pub fn spawn_text_box(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+) {
+    commands.spawn_bundle(NodeBundle {
+        style: Style {
+            position_type: PositionType::Absolute,
+            position: Rect { left: Val::Px(100.0), bottom: Val::Px(100.0), ..Default::default() },
+            padding: Rect::all(Val::Px(2.0)),
+            ..Default::default()
+        },
+        ..Default::default()
+    })
+    .spawn_child_bundle(NodeBundle {
+        color: UiColor (Color::DARK_GRAY),
+        style: Style {
+            padding: Rect::all(Val::Px(4.0)),
+            ..Default::default()
+        },
+        ..Default::default()
+    })
+    .spawn_child_bundle(TextBundle {
+        text: Text::with_section(
+            "Hello, world!",
+            TextStyle {
+                font: asset_server.load("FiraMono-Regular.ttf"),
+                font_size: 16.0,
+                color: Color::ANTIQUE_WHITE,
+            },
+            TextAlignment::default()
+        ),
+        ..Default::default()
+    });
+}
+```
+
+*I'm clueless about flexbox so don't expect this to work*
+
+# Haven't implemented BuildChildren yet, but this is fine:
+
+```rust
+fn spawn_hierachy(
+    mut commands: Commands
+) {
+    commands
+    .spawn_bundle(A)     
+    .with_children(|builder| {
+        builder
+        .spawn_bundle(B)
+        .spawn_child_bundle(C)
+        .spawn_child_bundle(D);
+
+        builder
+        .spawn_bundle(B)
+        .spawn_child_bundle(C)
+        .spawn_child_bundle(D);
+    });
+}
+
+```
+
+
+# Todo
+
+* Implement ChildBuilder.
+
+* Haven't considered performance at all.
+
+* Maybe some sort of spawn_brood function to spawn multiple children at once.
+
+* See if there is an ergonomic way to store entities ids in the middle of the hierachy.
+
---
+++ src/lib.rs
@@ -0,0 +1,246 @@
+use bevy::ecs::system::*;
+use bevy::prelude::*;
+
+pub struct ChildCommands<'w, 's, 'a> {
+    parent: Entity,
+    entity: Entity,
+    entity_commands: &'a mut EntityCommands<'w, 's, 'a>,
+}
+
+pub trait SpawnChild<'w, 's, 'a> {
+    fn spawn_child(&'a mut self) -> ChildCommands<'w, 's, 'a>;
+    fn spawn_child_bundle(&'a mut self, bundle: impl Bundle) -> ChildCommands<'w, 's, 'a> {
+        let mut child_commands = self.spawn_child();
+        child_commands.insert_bundle(bundle);
+        child_commands
+    }
+}
+
+impl<'w, 's, 'a> SpawnChild<'w, 's, 'a>  for EntityCommands<'w, 's, 'a> {
+    fn spawn_child(&'a mut self) -> ChildCommands<'w, 's, 'a> {
+        let parent = self.id();
+        let child = self.commands().spawn().id();
+        self.commands().add(AddChild { child, parent });
+        ChildCommands { 
+            parent,
+            entity: child,
+            entity_commands: self
+        }
+    } 
+}
+
+impl<'w, 's, 'a> SpawnChild<'w, 's, 'a> for ChildCommands<'w, 's, 'a> {
+    fn spawn_child(&'a mut self) -> ChildCommands<'w, 's, 'a> {
+        let parent = self.entity;
+        let child = self.commands().spawn().id();
+        self.commands().add(AddChild { child, parent });
+        ChildCommands {
+            parent,
+            entity: child,
+            entity_commands: self.entity_commands
+        }
+    }
+}
+
+impl <'w, 's, 'a> ChildCommands<'w, 's, 'a> {
+    #[inline]
+    #[must_use = "Omit the .id() call if you do not need to store the `Entity` identifier."]
+    pub fn id(&self) -> Entity {
+        self.entity
+    }
+
+    pub fn insert_bundle(&mut self, bundle: impl Bundle) -> &mut Self {
+        let entity = self.entity;
+        self.commands().add(InsertBundle {
+            entity,
+            bundle,
+        });
+        self
+    }
+
+    pub fn insert(&mut self, component: impl Component) -> &mut Self {
+        let entity = self.entity;
+        self.commands().add(Insert {
+            entity,
+            component,
+        });
+        self
+    }
+
+    pub fn spawn_sibling(&'a mut self) -> ChildCommands<'w, 's, 'a> {
+        let sibling = self.commands().spawn().id();
+        let parent = self.parent;
+        self.commands().add(AddChild { child: sibling, parent });
+        ChildCommands { 
+            parent,
+            entity: sibling,
+            entity_commands: self.entity_commands
+        }
+    }
+
+    pub fn spawn_sibling_bundle<T: Bundle>(&'a mut self, bundle: T) -> ChildCommands<'w, 's, 'a> {
+        let mut child_commands = self.spawn_sibling();
+        child_commands.insert_bundle(bundle);
+        child_commands
+    }
+
+    pub fn commands(&mut self) -> &mut Commands<'w, 's> {
+        self.entity_commands.commands()
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use crate::*;
+
+    #[derive(Component)]
+    struct A;
+
+    #[derive(Component)]
+    struct B;
+
+    #[derive(Component)]
+    struct C;
+
+    #[derive(Component)]
+    struct D;
+
+    #[derive(Component)]
+    struct E;
+
+    fn spawn_hierachy_1(
+        mut commands: Commands
+    ) {
+        commands
+        .spawn()
+        .insert(A)
+        .spawn_child()
+        .insert(B);
+    }
+
+    fn spawn_hierachy_2(
+        mut commands: Commands
+    ) {
+        commands
+        .spawn_bundle((A,))
+        .spawn_child_bundle((B,));
+    }
+
+    fn spawn_hierachy_3(
+        mut commands: Commands
+    ) {
+        commands
+        .spawn_bundle((A,))
+        .spawn_child_bundle((A, B,))
+        .spawn_child_bundle((A, B, C,));
+    }
+
+    fn spawn_hierachy_4(
+        mut commands: Commands
+    ) {
+        commands
+        .spawn_bundle((A,))
+        .spawn_child_bundle((A, B,))
+        .spawn_child_bundle((A, B, C,))
+        .spawn_sibling_bundle((A, B, C,))
+        .spawn_sibling_bundle((A, B, C,));
+    }
+
+    fn spawn_hierachy_5(
+        mut commands: Commands
+    ) {
+        let parent = commands
+        .spawn_bundle((A,))
+        .id();
+
+        commands.entity(parent)
+        .spawn_child_bundle((B,))
+        .spawn_child_bundle((C,))
+        .spawn_sibling_bundle((C,));
+        
+        commands.entity(parent)
+        .spawn_child_bundle((B,))
+        .spawn_child_bundle((C,))
+        .spawn_sibling_bundle((C,));    
+    }
+
+    fn spawn_hierachy_6(
+        mut commands: Commands
+    ) {
+        commands
+        .spawn_bundle((A,))     
+        .with_children(|builder| {
+            builder
+            .spawn_bundle((B,))
+            .spawn_child_bundle((C,))
+            .spawn_child_bundle((D,));
+
+            builder
+            .spawn_bundle((B,))
+            .spawn_child_bundle((C,))
+            .spawn_child_bundle((D,));;
+        });
+    }
+
+    #[test]
+    fn spawn_child_1() {
+        let mut world = World::default();
+        SystemStage::single_threaded().add_system(spawn_hierachy_1).run(&mut world);
+        assert_eq!(world.query::<Entity>().iter(&world).len(), 2);
+        let mut a = world.query::<(&Children, &A)>();
+        let mut b = world.query::<(&Parent, &B)>();
+        assert_eq!(a.iter(&world).len(), 1);
+        assert_eq!(b.iter(&world).len(), 1);
+        for (children, _) in a.iter(&world) {
+            for child in children.iter() {
+         
+                assert!(b.get(&world, *child).is_ok());
+            }
+        }
+    }
+
+    #[test]
+    fn spawn_child_2() {
+        let mut world = World::default();
+        SystemStage::single_threaded().add_system(spawn_hierachy_2).run(&mut world);
+        assert_eq!(world.query::<Entity>().iter(&world).len(), 2);
+        let mut a = world.query::<(&Children, &A)>();
+        let mut b = world.query::<(&Parent, &B)>();
+        assert_eq!(a.iter(&world).len(), 1);
+        assert_eq!(b.iter(&world).len(), 1);
+        for (children, _) in a.iter(&world) {
+            for child in children.iter() {
+                assert!(b.get(&world, *child).is_ok());
+            }
+        }
+    }
+
+    #[test]
+    fn spawn_child_3() {
+        let mut world = World::default();
+        SystemStage::single_threaded().add_system(spawn_hierachy_3).run(&mut world);
+        assert_eq!(world.query::<Entity>().iter(&world).len(), 3);
+        assert_eq!(world.query::<(&Children, &A)>().iter(&world).len(), 2);
+        assert_eq!(world.query::<(&Parent, &Children, &A, &B)>().iter(&world).len(), 1);
+        assert_eq!(world.query::<(&Parent, &A, &B, &C)>().iter(&world).len(), 1);
+    }
+
+    #[test]
+    fn spawn_child_4() {
+        let mut world = World::default();
+        SystemStage::single_threaded().add_system(spawn_hierachy_4).run(&mut world);
+        assert_eq!(world.query::<Entity>().iter(&world).len(), 5);
+        assert_eq!(world.query::<(&Children, &A)>().iter(&world).len(), 2);
+        assert_eq!(world.query::<(&Parent, &Children, &A, &B)>().iter(&world).len(), 1);
+        assert_eq!(world.query::<(&Parent, &A, &B, &C)>().iter(&world).len(), 3);
+    }
+
+    #[test]
+    fn spawn_child_5() {
+        let mut world = World::default();
+        SystemStage::single_threaded().add_system(spawn_hierachy_5).run(&mut world);
+        assert_eq!(world.query::<Entity>().iter(&world).len(), 7);
+        assert_eq!(world.query::<(&Parent, &C)>().iter(&world).len(), 4);
+    }
+}
